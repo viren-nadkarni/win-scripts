964985e39e56aef42c997bb4a649f3633e749992
viren nadkarni <viren@outlook.com>
2015-08-05 12:48:39 +0530

    argparse changes

--- sleep.py
+++ sleep.py
@@ -1,25 +1,33 @@
 __author__ = 'viren'
 
-import sys, os, time, subprocess, argparse
+import sys
+import os
+import time
+import subprocess
+import argparse
 
 def main():
     try:
         parser = argparse.ArgumentParser()
-        parser.add_argument('-t', help = 'set the time-out period in minutes', type = int)
-        parser.add_argument('-z', '--hibernate', help = 'hibernate instead of sleeping', action = 'store_true')
+        parser.add_argument('timeout', help='Timeout in minutes')
+        parser.add_argument('-s', '--sleep', help='Sleep (default)', action='store_true')
+        parser.add_argument('-z', '--hibernate', help='Hibernate', action='store_true')
         args = parser.parse_args()
 
-        c = args.t if args.t else 0
-        d = int(c) * 60
+        timeout_minutes = args.timeout if args.timeout else 0
+        timeout_seconds = int(timeout_minutes) * 60
 
-        if not c:
-            print 'No time-out specified'
-            sys.exit()
-        
-        for i in range(0, d):
-        	sys.stdout.write('Suspending in %d minutes %d seconds...\r'%((d - i) / 60, (d - i) % 60 ))
-        	sys.stdout.flush()
-        	time.sleep(1)
+        if args.hibernate:
+            action = 'Hibernating'
+        else:
+            action = 'Sleeping'
+            
+        for i in range(0, timeout_seconds):
+            sys.stdout.write('{} in {} minutes {} seconds...\r'.format(action, 
+                                                                       (timeout_seconds-i)/60, 
+                                                                       (timeout_seconds-i)%60 ))
+            sys.stdout.flush()
+            time.sleep(1)
 
         path = 'C:\\Windows\\System32\\rundll32.exe PowrProf.dll, SetSuspendState '
         if args.hibernate:
@@ -32,11 +40,15 @@ def main():
         print '\nKeyboardInterrupt: Operation cancelled'
         sys.exit()
 
-    except:
+    except Exception as e:
+        sys.stderr.write('Exception: ' + str(e) + '\n')
         sys.exit()
+
 
 if __name__ == '__main__':
     if os.name == 'nt':
         main()
     else:
+        sys.stderr.write('Unsupported OS\n')
         sys.exit()
+
